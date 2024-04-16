6e5014b67848391904a41bf141d33bddfb23231a
Vianney Lehu <60597553+RicqPascalv2@users.noreply.github.com>
2024-04-16 22:15:01 +0200

    finishing dashboard

--- src/components/DashboardVue.vue
+++ src/components/DashboardVue.vue
@@ -26,20 +26,35 @@
       <q-card-section class="q-pt-none">
       
       </q-card-section>
-
-      <q-separator inset />
-
-      <q-card-section>
-
+      <q-card-section class="q-pt-none">
+      <div>name: {{ manager.firstname }} {{ manager.lastname }}</div>
+      <div>email: {{ manager.email }}</div>
+      <div>phone: {{ manager.phone }}</div>
+      <!-- Add more fields as needed -->
       </q-card-section>
+
     </q-card>
     <q-card  v-if="curr_user.role == '1' || curr_user.role == '2'" flat bordered class="my-card">
       <q-card-section>
         <div class="text-h6">Mon prochain entretien personnel</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-     
+      <q-card-section class="">
+        <q-timeline layout="dense" side="alternate" color="secondary">
+          <q-timeline-entry
+            v-for="(entretien, index) in entretiens_perso"
+            :key="index"
+            :title="entretien.description"
+            :subtitle="entretien.date + ' ' + entretien.time"
+            :color="orange"
+            icon="event"
+          >
+          <div>
+            <br>
+            Avec: {{ users[entretien.managerId-1].firstname}} {{users[entretien.managerId-1].lastname}}
+          </div>
+          </q-timeline-entry>
+        </q-timeline>
       </q-card-section>
 
       <q-separator inset />
@@ -77,7 +92,7 @@
           >
           <div>
             <br>
-            Avec: {{ users[entretien.managedId-1].firstname}} {{users[entretien.managerId-1].lastname}}
+            Avec: {{ users[entretien.managedId-1].firstname}} {{users[entretien.managedId-1].lastname}}
           </div>
           </q-timeline-entry>
         </q-timeline>
@@ -104,15 +119,19 @@ const store = useLoginStore()
 const curr_user = store.getCurrentUser
 
 
-
 const entretiens = ref([])
+
+const manager = ref(usersData.find(user => user.id === curr_user.managerId))
+
+
+const entretiens_perso = ref(entretiensData.filter(entretien => entretien.managedId === curr_user.id))
 
 
 for (const entretien of entretiensData) {
   if (entretien.managerId === curr_user.id) {
     entretiens.value.push(entretien)
   }
-}
+} 
 
 console.log('entretiensData:', entretiensData)
 console.log('users:', users)
