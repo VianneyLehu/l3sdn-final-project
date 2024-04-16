b05656ae83d0e0a79dd140211711230d9e0f70ec
axel59830 <axel.krebs145@gmail.com>
2024-04-16 18:41:29 +0200

    navbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,16 @@
 <template>
-  <p> bonjour </p>
+  <div>
+    <NavBar /> <!-- Composant de la barre de navigation -->
+    <router-view /> <!-- Contenu de la page -->
+  </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue' // Importer le composant de la barre de navigation
+
+export default {
+  components: {
+    NavBar // Enregistrer le composant pour pouvoir l'utiliser dans ce layout
+  }
+}
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -28,3 +28,4 @@ export default route(function (/* { store, ssrContext } */) {
 
   return Router
 })
+
