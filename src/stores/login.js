feb8382634d9dd6263e6b135c2d66c675bd12f03
Zaouich123 <etienne.baillie@gmail.com>
2024-04-16 10:07:33 +0200

    stores with the new json broken

--- src/stores/login.js
+++ src/stores/login.js
@@ -1,31 +1,31 @@
 import { store } from 'quasar/wrappers'
 import { defineStore } from 'pinia'
+import { users } from '../data/users.json'
 
 
 export const useLoginStore = defineStore('login', {
-    state: () => ({ log : [ {firstname : 't', name : 't', email : 't', phone : 't', password : 't', role : 'RH', manager : ''}
-  ], currentUser: null }),
+    state: () => ({ currentUser: null }),
     getters: {
-      doubleCount: (state) => state.count * 2,
     },
     actions: {
       registerUser(firstname,name, email, phone, password) {
+
         // i want to push the data to the state like firstname : 'firstname', name : 'name', email : 'email', phone : 'phone', password : 'password'
         // For example if i want to get the firstname i will do store.state.login.firstname but if another one want to register then that push another data to the state
-        this.log.push({ firstname : firstname, name : name, email : email, phone : phone, password : password, role : 'RH', manager : ''})
-        
+        users.employees.push({ firstname : firstname, lastname : name, email : email, phone : phone, password : password, role : '1', manage : [] })
         
       },
         loginUser(emailphone, password) {
-          this.log.forEach(element => {
+          users.employees.forEach(element => {
             if ((element.email == emailphone || element.phone == emailphone) && element.password == password) {
               console.log('Login success')
               this.currentUser = element
               return true
           } 
+          
+          })
           console.log('Login failed')
           return false 
-          })
         },
         logoutUser() {
           this.currentUser = null
